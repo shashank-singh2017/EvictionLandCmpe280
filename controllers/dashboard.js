633b0eccdf440a6a250d85ad70e33d8f08ae358b
Divya Thazhathethil <DivyaKalidasan.Thazhathethil@sjsu.edu>
2018-11-22 16:08:39 -0800

    Dashboard map state highlight

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,64 +1,82 @@
 var xlsx = require('xlsx');
+var states = xlsx.readFile('./public/data/states.xlsx').Sheets.Sheet1;
 const monk = require('monk');
 const url = 'localhost:27017/cmpe280';
 const db = monk(url);
 
 
-module.exports.fetchData = function(req,res) {
+module.exports.fetchData = function (req, res) {
     const cases = db.get('USData');
 
-    cases.find({"year":2016}).then((results)=>{
+    cases.find({"year": 2016}).then((results) => {
         res.render('../views/dashboard', {
-            'data':results
+            'data': results,
+            'state': ""
         });
     });
 };
 
-module.exports.fetchDataByState = function(req,res) {
-    const selectedState = req.session.userSession[0].state;
+module.exports.fetchDataByState = function (req, res) {
+    //const selectedState = req.session.userSession[0].state;
+    const selectedState = "California";
+
     console.log(selectedState);
     const cases = db.get('USData');
     var dataJsonArray = [];
     var dataAllYearsOfState = [];
     var rentBurdenAllYears = [];
-    var poverty =[];
+    var poverty = [];
     cases.find(
-            { "year":2016 }
-
-    ).then((results)=>{
-        for(var i= 0; i < results.length; i++){
+        {"year": 2016}
+    ).then((results) => {
+        for (var i = 0; i < results.length; i++) {
             dataJsonArray.push({
                 state: results[i].name,
                 value: results[i].evictions
             });
         }
 
+        cases.find({"name": selectedState}).then((results1) => {
+            for (var i = 0; i < results1.length; i++) {
+                dataAllYearsOfState.push({
+                    year: results1[i].year,
+                    value: results1[i].evictions
+                });
 
-        cases.find({"name": selectedState}).then((results1)=>{
-                for(var i= 0; i < results1.length; i++){
-                    dataAllYearsOfState.push({
-                            year: results1[i].year,
-                            value: results1[i].evictions
-                    });
+                rentBurdenAllYears.push({
+                    year: results1[i].year,
+                    value: results1[i]["rent-burden"]
+                });
 
-                    rentBurdenAllYears.push({
-                        year: results1[i].year,
-                        value: results1[i]["rent-burden"]
-                    });
+                poverty.push({
+                    year: results1[i].year,
+                    poverty: results1[i]["poverty-rate"],
+                    population: results1[i]["population"]
+                });
+            }
 
-                    poverty.push({
-                        year: results1[i].year,
-                        poverty: results1[i]["poverty-rate"],
-                        population : results1[i]["population"]
+            var statesJsonArray = xlsx.utils.sheet_to_json(states);
+            const cases = db.get('reportedCases');
+            var rankingsJsonArray = [];
+
+            cases.find({}, {sort: {State_Reported_Cases: -1}, limit:3}).then((results)=>{
+                for(var i= 0; i < results.length; i++){
+                    rankingsJsonArray.push({
+                        rank: i+1,
+                        state: results[i].state,
+                        county: results[i].county,
+                        case_numbers: results[i].case_numbers
                     });
                 }
-            res.render('../views/dashboard', {'mapData':dataJsonArray,
-                'yearData':dataAllYearsOfState,
-                'state': selectedState,
-                'rentBurden': rentBurdenAllYears,
-                'poverty' : poverty
+                res.render('../views/dashboard', {
+                    'rankings':rankingsJsonArray,
+                    'mapData': dataJsonArray,
+                    'yearData': dataAllYearsOfState,
+                    'state': selectedState,
+                    'rentBurden': rentBurdenAllYears,
+                    'poverty': poverty
+                });
             });
-
         });
 
 
