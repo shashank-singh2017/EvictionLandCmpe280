cd4a81ab68011baa4fe7d7134cd095a9af62d2b2
Divya Thazhathethil <DivyaKalidasan.Thazhathethil@sjsu.edu>
2018-11-25 12:17:30 -0800

    updated group bar chart

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -19,23 +19,20 @@ module.exports.fetchData = function (req, res) {
 module.exports.fetchDataByState = function (req, res) {
     //const selectedState = req.session.userSession[0].state;
     const selectedState = "California";
-
     console.log(selectedState);
     const cases = db.get('USData');
     var dataJsonArray = [];
     var dataAllYearsOfState = [];
     var rentBurdenAllYears = [];
+    var stackedChartData = [];
     var poverty = [];
-    cases.find(
-        {"year": 2016}
-    ).then((results) => {
+    cases.find({"year": 2016}).then((results) => {
         for (var i = 0; i < results.length; i++) {
             dataJsonArray.push({
                 state: results[i].name,
                 value: results[i].evictions
             });
         }
-
         cases.find({"name": selectedState}).then((results1) => {
             for (var i = 0; i < results1.length; i++) {
                 dataAllYearsOfState.push({
@@ -53,16 +50,33 @@ module.exports.fetchDataByState = function (req, res) {
                     poverty: results1[i]["poverty-rate"],
                     population: results1[i]["population"]
                 });
+
+                if(results1[i].year %2 == 0){
+                    var values = [];
+                    values.push({
+                        value : results1[i]["rent-burden"] / 4,
+                        rate : "Rent burden"
+                    });
+                    values.push({
+                        value : results1[i]["eviction-rate"],
+                        rate : "Eviction rate"
+                    });
+                    stackedChartData.push({
+                        categorie : results1[i].year,
+                        values : values
+                    });
+                }
+
             }
 
             var statesJsonArray = xlsx.utils.sheet_to_json(states);
             const rankings = db.get('reportedCases');
             var rankingsJsonArray = [];
 
-            rankings.find({}, {sort: {State_Reported_Cases: -1}, limit:3}).then((results)=>{
-                for(var i= 0; i < results.length; i++){
+            rankings.find({}, {sort: {State_Reported_Cases: -1}, limit: 3}).then((results) => {
+                for (var i = 0; i < results.length; i++) {
                     rankingsJsonArray.push({
-                        rank: i+1,
+                        rank: i + 1,
                         state: results[i].state,
                         county: results[i].county,
                         case_numbers: results[i].case_numbers
@@ -71,24 +85,25 @@ module.exports.fetchDataByState = function (req, res) {
 
                 var rank = 0;
                 var evictionRate = 0;
-                cases.find({"year": 2016}, {sort : {evictions : -1}}).then((results2)=>{
-                    for(var i= 0; i < results2.length; i++){
-                        if( results2[i].name == selectedState){
-                            rank = i ;
+                cases.find({"year": 2016}, {sort: {evictions: -1}}).then((results2) => {
+                    for (var i = 0; i < results2.length; i++) {
+                        if (results2[i].name == selectedState) {
+                            rank = i;
                             evictionRate = results2[i].evictions;
                         }
                     }
                     res.render('../views/dashboard', {
-                        'rankings':rankingsJsonArray,
+                        'rankings': rankingsJsonArray,
                         'mapData': dataJsonArray,
                         'yearData': dataAllYearsOfState,
                         'state': selectedState,
                         'rentBurden': rentBurdenAllYears,
                         'poverty': poverty,
-                        'stateData' : {
-                            'rank':rank,
-                            'evictionRate' : evictionRate
-                        }
+                        'stateData': {
+                            'rank': rank,
+                            'evictionRate': evictionRate
+                        },
+                        'stackedChartData' : stackedChartData
                     });
                 });
 
